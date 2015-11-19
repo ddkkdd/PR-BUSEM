c325fcbe2609bbcdeb4f86b5d4e5699878302bc1
ddkkdd <daniel.kaltenbrunner@gmail.com>
2015-11-19 17:22:55 +0100

    temp

--- ModelWhileWorkV2/Controllers/ProcessModelController.cs
+++ ModelWhileWorkV2/Controllers/ProcessModelController.cs
@@ -5,52 +5,68 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using ModelWhileWorkV2.Models;
+using System.Xml.Serialization;
+using System.IO;
 
-namespace ModelWhileWorkV1.Controllers
+namespace ModelWhileWorkV2.Controllers
 {
     public class ProcessModelController : ApiController
     {
-        List<XmlSbpmEntireModel> test = new List<XmlSbpmEntireModel>()
+        //List<XmlSbpmEntireModel> test = new List<XmlSbpmEntireModel>()
+        //{
+        //    new XmlSbpmEntireModel()
+        //    {
+        //        ProcessName = "MyFirstProcess",
+        //        Version = "1.0.0"
+        //    },
+
+        //    new XmlSbpmEntireModel()
+        //    {
+        //        ProcessName = "MySecondProcess",
+        //        Version = "1.0.0"
+        //    },
+
+        //    new XmlSbpmEntireModel()
+        //    {
+        //        ProcessName = "MyThirdProcess",
+        //        Version = "1.0.0"
+        //    }
+        //};
+
+        //public IEnumerable<XmlSbpmEntireModel> GetAllEntireModels()
+        //{
+        //    return test;
+        //}
+
+        //public IHttpActionResult GetEntireModel(int id)
+        //{
+        //    var model = test.ElementAt(id - 1);
+
+        //    if (model != null)
+        //    {
+        //        return Ok(model);
+        //    }
+
+        //    return NotFound();
+        //}
+
+        public IHttpActionResult GetModel()
         {
-            new XmlSbpmEntireModel()
+            try
             {
-                ProcessName = "MyFirstProcess",
-                Version = "1.0.0"
-            },
+                XmlSerializer serializer = new XmlSerializer(typeof(XmlSbpmEntireModel));
 
-            new XmlSbpmEntireModel()
-            {
-                ProcessName = "MySecondProcess",
-                Version = "1.0.0"
-            },
+                using (FileStream fileStream = new FileStream("Upload/prozess132.exml", FileMode.Open))
+                {
+                    XmlSbpmEntireModel processModelObject = (XmlSbpmEntireModel)serializer.Deserialize(fileStream);
 
-            new XmlSbpmEntireModel()
-            {
-                ProcessName = "MyThirdProcess",
-                Version = "1.0.0"
+                    return Ok(processModelObject);
+                }
             }
-        };
-
-        public IEnumerable<XmlSbpmEntireModel> GetAllEntireModels()
-        {
-            return test;
-        }
-
-        public IHttpActionResult GetEntireModel(int id)
-        {
-            var model = test.ElementAt(id - 1);
-
-            if (model != null)
+            catch (IOException ex)
             {
-                return Ok(model);
+                return InternalServerError(ex);
             }
-
-            return NotFound();
-        }
-
-        public void serializeProcessModel()
-        {
-
         }
     }
 }
