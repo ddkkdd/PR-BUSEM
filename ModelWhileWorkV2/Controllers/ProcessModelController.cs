6cb559166cacded621b7b998b110d4c1c32703c7
ddkkdd <daniel.kaltenbrunner@gmail.com>
2015-11-19 16:15:26 +0100

    Ready to implement XML serialization

---
+++ ModelWhileWorkV2/Controllers/ProcessModelController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ModelWhileWorkV2.Models;
+
+namespace ModelWhileWorkV1.Controllers
+{
+    public class ProcessModelController : ApiController
+    {
+        List<XmlSbpmEntireModel> test = new List<XmlSbpmEntireModel>()
+        {
+            new XmlSbpmEntireModel()
+            {
+                ProcessName = "MyFirstProcess",
+                Version = "1.0.0"
+            },
+
+            new XmlSbpmEntireModel()
+            {
+                ProcessName = "MySecondProcess",
+                Version = "1.0.0"
+            },
+
+            new XmlSbpmEntireModel()
+            {
+                ProcessName = "MyThirdProcess",
+                Version = "1.0.0"
+            }
+        };
+
+        public IEnumerable<XmlSbpmEntireModel> GetAllEntireModels()
+        {
+            return test;
+        }
+
+        public IHttpActionResult GetEntireModel(int id)
+        {
+            var model = test.ElementAt(id - 1);
+
+            if (model != null)
+            {
+                return Ok(model);
+            }
+
+            return NotFound();
+        }
+
+        public void serializeProcessModel()
+        {
+
+        }
+    }
+}
---
+++ ModelWhileWorkV2/Models/ProcessModel.cs
@@ -0,0 +1,469 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ModelWhileWorkV2.Models
+{
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.0.30319.33440")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlRootAttribute("EntireModel", Namespace = "", IsNullable = false)]
+    public partial class XmlSbpmEntireModel
+    {
+
+        private XmlSbpmEntireModelSubject[] subjectField;
+
+        private string processNameField;
+
+        private string versionField;
+
+        public XmlSbpmEntireModel()
+        {
+            this.processNameField = "";
+            this.versionField = "";
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Subject", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public XmlSbpmEntireModelSubject[] Subject
+        {
+            get
+            {
+                return this.subjectField;
+            }
+            set
+            {
+                this.subjectField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        [System.ComponentModel.DefaultValueAttribute("")]
+        public string ProcessName
+        {
+            get
+            {
+                return this.processNameField;
+            }
+            set
+            {
+                this.processNameField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        [System.ComponentModel.DefaultValueAttribute("")]
+        public string Version
+        {
+            get
+            {
+                return this.versionField;
+            }
+            set
+            {
+                this.versionField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.0.30319.33440")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlRootAttribute("Subject", Namespace = "", IsNullable = false)]
+    public partial class XmlSbpmEntireModelSubject
+    {
+
+        private XmlSbpmModelingElement[] elementField;
+
+        private XmlSbpmConnection[] connectionField;
+
+        private string subjectNameField;
+
+        private string realNameField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Element", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public XmlSbpmModelingElement[] Element
+        {
+            get
+            {
+                return this.elementField;
+            }
+            set
+            {
+                this.elementField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Connection", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public XmlSbpmConnection[] Connection
+        {
+            get
+            {
+                return this.connectionField;
+            }
+            set
+            {
+                this.connectionField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string SubjectName
+        {
+            get
+            {
+                return this.subjectNameField;
+            }
+            set
+            {
+                this.subjectNameField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string RealName
+        {
+            get
+            {
+                return this.realNameField;
+            }
+            set
+            {
+                this.realNameField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlIncludeAttribute(typeof(XmlReceiveElement))]
+    [System.Xml.Serialization.XmlIncludeAttribute(typeof(XmlSendElement))]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.0.30319.33440")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    public partial class XmlSbpmModelingElement : XmlSbpmModelingObject
+    {
+
+        private float xField;
+
+        private float yField;
+
+        private float angleField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public float x
+        {
+            get
+            {
+                return this.xField;
+            }
+            set
+            {
+                this.xField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public float y
+        {
+            get
+            {
+                return this.yField;
+            }
+            set
+            {
+                this.yField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public float angle
+        {
+            get
+            {
+                return this.angleField;
+            }
+            set
+            {
+                this.angleField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlIncludeAttribute(typeof(XmlCoreConnection))]
+    [System.Xml.Serialization.XmlIncludeAttribute(typeof(XmlSbpmConnection))]
+    [System.Xml.Serialization.XmlIncludeAttribute(typeof(XmlSbpmModelingElement))]
+    [System.Xml.Serialization.XmlIncludeAttribute(typeof(XmlReceiveElement))]
+    [System.Xml.Serialization.XmlIncludeAttribute(typeof(XmlSendElement))]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.0.30319.33440")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    public abstract partial class XmlSbpmModelingObject
+    {
+
+        private string uUIDField;
+
+        private string nameField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public string UUID
+        {
+            get
+            {
+                return this.uUIDField;
+            }
+            set
+            {
+                this.uUIDField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public string name
+        {
+            get
+            {
+                return this.nameField;
+            }
+            set
+            {
+                this.nameField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlIncludeAttribute(typeof(XmlSbpmXmppMessage))]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.0.30319.33440")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    public partial class XmlCoreXmppMessage
+    {
+
+        private string messageField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public string message
+        {
+            get
+            {
+                return this.messageField;
+            }
+            set
+            {
+                this.messageField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.0.30319.33440")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    public partial class XmlSbpmXmppMessage : XmlCoreXmppMessage
+    {
+
+        private string recipientField;
+
+        private string senderField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public string recipient
+        {
+            get
+            {
+                return this.recipientField;
+            }
+            set
+            {
+                this.recipientField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public string sender
+        {
+            get
+            {
+                return this.senderField;
+            }
+            set
+            {
+                this.senderField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlIncludeAttribute(typeof(XmlSbpmConnection))]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.0.30319.33440")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    public partial class XmlCoreConnection : XmlSbpmModelingObject
+    {
+
+        private bool directed1Field;
+
+        private bool directed2Field;
+
+        private XmlSbpmModelingElement endPoint1Field;
+
+        private XmlSbpmModelingElement endPoint2Field;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public bool directed1
+        {
+            get
+            {
+                return this.directed1Field;
+            }
+            set
+            {
+                this.directed1Field = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public bool directed2
+        {
+            get
+            {
+                return this.directed2Field;
+            }
+            set
+            {
+                this.directed2Field = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public XmlSbpmModelingElement endPoint1
+        {
+            get
+            {
+                return this.endPoint1Field;
+            }
+            set
+            {
+                this.endPoint1Field = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public XmlSbpmModelingElement endPoint2
+        {
+            get
+            {
+                return this.endPoint2Field;
+            }
+            set
+            {
+                this.endPoint2Field = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.0.30319.33440")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    public partial class XmlSbpmConnection : XmlCoreConnection
+    {
+
+        private XmlSbpmXmppMessage msgField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public XmlSbpmXmppMessage msg
+        {
+            get
+            {
+                return this.msgField;
+            }
+            set
+            {
+                this.msgField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.0.30319.33440")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    public partial class XmlReceiveElement : XmlSbpmModelingElement
+    {
+
+        private XmlSbpmXmppMessage[] messagesField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlArrayAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        [System.Xml.Serialization.XmlArrayItemAttribute("msg", Form = System.Xml.Schema.XmlSchemaForm.Unqualified, IsNullable = false)]
+        public XmlSbpmXmppMessage[] messages
+        {
+            get
+            {
+                return this.messagesField;
+            }
+            set
+            {
+                this.messagesField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.0.30319.33440")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    public partial class XmlSendElement : XmlSbpmModelingElement
+    {
+
+        private XmlSbpmXmppMessage msgField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public XmlSbpmXmppMessage msg
+        {
+            get
+            {
+                return this.msgField;
+            }
+            set
+            {
+                this.msgField = value;
+            }
+        }
+    }
+}
