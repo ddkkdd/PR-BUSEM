bc34a03762c8e49287d88ab9829175a1a9690b7f
ddkkdd <daniel.kaltenbrunner@gmail.com>
2016-01-30 15:08:07 +0100

    commit

--- ModelWhileWorkV2/Scripts/Model/MergeModelSteps.js
+++ ModelWhileWorkV2/Scripts/Model/MergeModelSteps.js
@@ -2,17 +2,33 @@
 {
     var chkBoxesModel1 = document.getElementsByClassName("chkBox1");
     var chkBoxesModel1checked = [];
+    var model1ElemIds = [];
+    var model1Elements = [];
     
     var chkBoxesModel2 = document.getElementsByClassName("chkBox2");
     var chkBoxesModel2checked = [];
+    var model2ElemIds = [];
+    var model2Elements = [];
 
     chkBoxesModel1checked = checkWhichCheckBoxIsChecked(chkBoxesModel1);
     chkBoxesModel2checked = checkWhichCheckBoxIsChecked(chkBoxesModel2);
 
+    model1ElemIds = getElementIdsOfCheckBoxChecked(chkBoxesModel1checked);
+    model2ElemIds = getElementIdsOfCheckBoxChecked(chkBoxesModel2checked);
     
+    var commonName = "";
 
-    console.log(chkBoxesModel1checked);
-    console.log(chkBoxesModel2checked);
+    if (chkBoxesModel1checked.length == 1)
+    {
+//        commonName = chkBoxesModel1checked[0].
+    }
+
+    if (chkBoxesModel2checked.length == 1) 
+    {
+    //    commonName = chkBoxesModel1checked[0].
+    }
+    console.log(model1Elements);
+    console.log(model2Elements);
 }
 
 function checkWhichCheckBoxIsChecked (chkBoxes)
@@ -28,4 +44,37 @@ function checkWhichCheckBoxIsChecked (chkBoxes)
     }
 
     return checked;
+}
+
+function getElementIdsOfCheckBoxChecked (chkBoxesChecked)
+{
+    var modelIds = [];
+    for (var i = 0; i < chkBoxesChecked.length; i++) {
+        var tmp = chkBoxesChecked[i].id.split(":");
+        var value = parseInt(tmp[1], 10);
+        modelIds.push(value);
+    }
+
+    return modelIds;
+}
+
+function getElementById(modelNr, id)
+{
+    return document.getElementById(modelNr+":"+id);
+}
+
+function getElementsById(modelNr, modelElemIds)
+{
+    var elements = [];
+    console.log(modelElemIds);
+
+    for (var i=0; i<modelElemIds.length; i++)
+    {
+        console.log(modelElemIds[i]);
+        elements.push (getElementById(
+                modelNr,
+                modelElemIds[i]));
+    }
+
+    return elements;
 }
